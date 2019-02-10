60e880d1b2c87fccd1b6a2686b0b64b369c63915
Keven Imbeault <imbk250998@tech-info.local>
2019-02-10 18:10:09 -0500

    Added Web_Application

---
+++ Web_Application/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Projets';
+  
+  constructor(private httpService: HttpClient) { }
+  array: string [];
+
+  ngOnInit() {
+    this.httpService.get('http://127.0.0.1:5000/api/github').subscribe(
+      data => {
+        this.array = data as string [];
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err.message);
+      }
+    );
+  }
+
+
+
+}
