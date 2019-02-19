961d91f78775e7f6d97a72a8a1604f52794edfe7
Keven Imbeault <xkeke111x@gmail.com>
2019-02-19 13:59:14 -0500

    Updated the database updater to remove the github key

--- DB/DB_Updater.py
+++ DB/DB_Updater.py
@@ -6,8 +6,10 @@ import time
 conn = sqlite3.connect('web.db')
 print("Script started !")
 
+f = open('Github_Key.txt', 'r')
+
 #TODO Encrypt this key
-git = Github("fb3025d2855730d6b8d29ff75d0dbefa35f0d99c")
+git = Github(f.readline())
 
 
 def job():
@@ -20,25 +22,27 @@ def job():
 
     #Gets public repos of the user and replaces empty descriptions with "TODO".
     for repo in git.get_user().get_repos():
+        print(repo.language)
         if repo.description == None:
-            Repo_Info = [repo.name, "TODO", repo.html_url, str(repo.forks_count), str(repo.stargazers_count)]
+            Repo_Info = [repo.name, "TODO", repo.html_url, str(repo.forks_count), str(repo.stargazers_count), repo.updated_at.strftime('%Y-%m-%d'), repo.language]
         else:
-            Repo_Info = [repo.name, repo.description, repo.html_url, str(repo.forks_count), str(repo.stargazers_count)]
+            Repo_Info = [repo.name, repo.description, repo.html_url, str(repo.forks_count), str(repo.stargazers_count), repo.updated_at.strftime('%Y-%m-%d'), repo.language]
         Repos.append(Repo_Info)
 
     for repo in Repos:
+        
         Repos_Names.append(repo[0])
         cursor.execute("SELECT count(*) FROM GITHUB WHERE NAME = ?", (repo[0],))
         data=cursor.fetchone()[0]
         #If the repo isn't in the GITHUB table, insert it.
         if data==0:
-            params = (repo[0], repo[1], repo[2], repo[3], repo[4])
-            cursor.execute("INSERT INTO GITHUB (NAME, DESCRIPTION, LINK, FORKS, STARS) VALUES (?, ?, ?, ?, ?)", params)
+            params = (repo[0], repo[1], repo[2], repo[3], repo[4], repo[5], repo[6])
+            cursor.execute("INSERT INTO GITHUB (NAME, DESCRIPTION, LINK, FORKS, STARS, UPDATED_AT, LANGUAGE) VALUES (?, ?, ?, ?, ?, ?, ?)", params)
             print("Created an entry for the repo : " + repo[0])
         #If the repo is already in the GITHUB table, update it.
         else:
-            params = (repo[1], repo[2], repo[3], repo[4], repo[0])
-            cursor.execute("UPDATE GITHUB set DESCRIPTION = ?, LINK = ?, FORKS = ?, STARS = ? where NAME = ?", params)
+            params = (repo[1], repo[2], repo[3], repo[4], repo[5], repo[6], repo[0])
+            cursor.execute("UPDATE GITHUB set DESCRIPTION = ?, LINK = ?, FORKS = ?, STARS = ?, UPDATED_AT = ?, LANGUAGE = ? WHERE NAME = ?", params)
             print("Updated the entry for the repo : " + repo[0])
 
     #Get the names of the repos inside the GITHUB table and puts them in a list .   
@@ -59,7 +63,7 @@ def job():
     
     
 #Do the function job every hour.
-schedule.every().hour.do(job)
+schedule.every().minute.do(job)
 
 while True:
     schedule.run_pending()
